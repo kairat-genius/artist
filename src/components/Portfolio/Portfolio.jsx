fa2b3f0d325be84cec7b0b25703f8c8632ec520f
kairat <k_kurmanov@list.ru>
2024-10-02 15:02:24 +0600

    checkbox, translation

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -140,6 +140,45 @@ const Portfolio = ({ home, Category }) => {
     }
   }, [data]);
 
+  // перевод дней
+
+  const getDeclension = (number, singular, pluralFew, pluralMany) => {
+    const lastDigit = number % 10;
+    const lastTwoDigits = number % 100;
+
+    if (lastTwoDigits >= 11 && lastTwoDigits <= 19) {
+      return pluralMany;
+    }
+
+    if (lastDigit === 1) {
+      return singular;
+    }
+
+    if (lastDigit >= 2 && lastDigit <= 4) {
+      return pluralFew;
+    }
+
+    return pluralMany;
+  };
+
+  // Функция для определения правильного склонения в зависимости от единицы времени
+  const getTimeUnit = (value, unit) => {
+    switch (unit) {
+      case "days":
+        return getDeclension(value, "день", "дня", "дней");
+      case "weeks":
+        return getDeclension(value, "неделя", "недели", "недель");
+      case "months":
+        return getDeclension(value, "месяц", "месяца", "месяцев");
+      case "hours":
+        return getDeclension(value, "час", "часа", "часов");
+      case "minutes":
+        return getDeclension(value, "минута", "минуты", "минут");
+      default:
+        return unit;
+    }
+  };
+
   return (
     <section className={home ? "portfolio" : "portfolio-cat"} id="gallery">
       <div className="portfolio_content">
@@ -195,7 +234,8 @@ const Portfolio = ({ home, Category }) => {
                     Площадь: {dataDetail.area} {dataDetail.areaUnit}
                   </p>
                   <p>
-                    Срок: {dataDetail.dueDate} {dataDetail.dueDateUnit}
+                  Срок: {dataDetail.dueDate}{" "}
+                  {getTimeUnit(dataDetail.dueDate, dataDetail.dueDateUnit)}
                   </p>
                 </div>
               </div>
